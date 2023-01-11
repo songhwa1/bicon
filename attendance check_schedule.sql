8e7342cd7b5e5a58124b71950af00e90090c941a
songhwa1 <121270461+songhwa1@users.noreply.github.com>
2023-01-11 21:15:57 +0900

    Add files via upload

--- attendance check_schedule.sql
+++ attendance check_schedule.sql
@@ -36,7 +36,7 @@ CREATE TABLE `schedule` (
 
 LOCK TABLES `schedule` WRITE;
 /*!40000 ALTER TABLE `schedule` DISABLE KEYS */;
-INSERT INTO `schedule` VALUES ('오송화','asdf','2023-01-10','fdsfsdsfs'),('이상복','boki',NULL,NULL),('오송화','asdf','2023-01-10','dsfsdfdfs'),('오송화','asdf','2023-01-10','tdgfdgsgsd'),('오송화','asdf','2023-01-10','어려워'),('오송화','asdf','2023-01-10','fcxvzvxcv'),('오송화','asdf','2023-01-10','gdgfsd');
+INSERT INTO `schedule` VALUES ('오송화','asdf','2023-01-10','fdsfsdsfs'),('이상복','boki',NULL,NULL),('오송화','asdf','2023-01-10','dsfsdfdfs'),('오송화','asdf','2023-01-10','tdgfdgsgsd'),('오송화','asdf','2023-01-10','어려워'),('오송화','asdf','2023-01-10','fcxvzvxcv'),('오송화','asdf','2023-01-10','gdgfsd'),('오송화','asdf','2023-01-11','gfhhd'),('오송화','asdf','2023-01-11','gfhhdfd\nfdgfgsdfg\n'),('김성일','ksi',NULL,NULL),('김성일','ksi','2023-01-11','되나?'),('김성일','ksi','2023-01-11','되나?\n추가도 됨?');
 /*!40000 ALTER TABLE `schedule` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-10 21:27:37
+-- Dump completed on 2023-01-11 21:12:58
